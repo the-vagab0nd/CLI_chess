1f14783d1a179463df2d96b9b865b418d33b9223
the-vagab0nd <54442577+the-vagab0nd@users.noreply.github.com>
2020-07-23 20:26:26 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <vector>
 #include <math.h>
-
+const int sz = 8;
 using namespace std;
 void ColorPrint(const char text, int fg_color, int bg_color)
 {
@@ -15,7 +15,6 @@ class Board;
 class Box;
 class Piece  {
     public :
-        bool is_killed = false; 
         bool is_white = false;
         char c;
         Piece(bool is_white, char c){
@@ -25,14 +24,63 @@ class Piece  {
         bool isAlive(){
             return this->is_killed == false;
         }
-        void setKilled(){
-            is_killed = true;
-        }
         bool colour(){
             return this->is_white;
         }
         char name(){
             return c;
+        }
+        void rookMoves(vector<vector<Piece *> >boxes, pair<int, int> start, vector<vector<int> > &arr){
+            int c = 1;
+            while(start.second + c < sz){
+                arr[start.first][start.second+c]++;
+                if(boxes[start.first][start.second+c] != NULL)break;
+                c++;
+            }
+            c = 1;
+            while(c <= start.second){
+                arr[start.first][start.second-c]++;
+                if(boxes[start.first][start.second-c] != NULL)break;
+                c++;
+            }
+            c = 1;
+            while(start.first + c < sz){
+                arr[start.first+c][start.second]++;
+                if(boxes[start.first+c][start.second] != NULL)break;
+                c++;
+            }
+            c = 1;
+            while(c <= start.first){
+                arr[start.first-c][start.second]++;
+                if(boxes[start.first-c][start.second] != NULL)break;
+                c++;
+            }
+        }
+        void bishopMoves(vector<vector<Piece *> >boxes, pair<int, int> start, vector<vector<int> > &arr){
+            int c = 1;
+            while(start.first + c < sz and start.second + c < sz){
+                arr[start.first+c][start.second+c]++;
+                if(boxes[start.first + c][start.second+c] != NULL)break;
+                c++;
+            }
+            c = 1;
+            while(start.first + c < sz and start.second - c >= 0){
+                arr[start.first+c][start.second-c]++;
+                if(boxes[start.first + c][start.second-c] != NULL)break;
+                c++;
+            }
+            c = 1;
+            while(start.first -c >= 0 and start.second + c < sz){
+                arr[start.first-c][start.second+c]++;
+                if(boxes[start.first - c][start.second+c] != NULL)break;
+                c++;
+            }
+            c = 1;
+            while(start.first -c >= 0 and start.second - c >= 0){
+                arr[start.first-c][start.second-c]++;
+                if(boxes[start.first - c][start.second - c] != NULL)break;
+                c++;
+            }
         }
         virtual bool canMove(vector<vector<Piece *> >boxes, pair<int, int> start, pair<int, int> end) = 0;
 };
@@ -54,21 +102,28 @@ class Rook : public Piece{
     public :
         Rook(bool is_white):Piece(is_white, 'r'){};
         bool canMove(vector<vector<Piece *> >boxes, pair<int, int> start, pair<int, int> end){
-            return true;
+            vector<vector<int> >arr(8, vector<int>(8, 0));
+            rookMoves(boxes,start, arr);    
+            return arr[end.first][end.second] > 0;
         }
 };
 class Bishop : public Piece{
     public :
         Bishop(bool is_white):Piece(is_white, 'b'){};
         bool canMove(vector<vector<Piece *> >boxes, pair<int, int> start, pair<int, int> end){
-            return true;
+            vector<vector<int> >arr(8, vector<int>(8, 0));
+            bishopMoves(boxes, start, arr);
+            return arr[end.first][end.second] > 0;
         }
 };
 class King : public Piece{
     public :
         King(bool is_white):Piece(is_white, 'K'){};
         bool canMove(vector<vector<Piece *> >boxes, pair<int, int> start, pair<int, int> end){
-            return true;
+            vector<vector<int> >arr(8, vector<int>(8, 0));
+            bishopMoves(boxes, start, arr);
+            rookMoves(boxes,start, arr); 
+            return arr[end.first][end.second] > 0;
         }
 };
 class Queen : public Piece{
@@ -93,12 +148,12 @@ class Board {
     public :
         Board(){
             boxes.resize(8, vector<Piece*>(8));
-            for(int i = 0; i < 8; i++){
+            for(int i = 0; i < sz; i++){
                 boxes[1][i] = new Pawn(1);
                 boxes[6][i] = new Pawn(0);
             }
-            for(int i = 2; i < 6; i++){
-                for(int j = 0; j < 8; j++){
+            for(int i = 2; i < sz-2; i++){
+                for(int j = 0; j < sz; j++){
                     boxes[i][j] = NULL;
                 }
             }
@@ -140,12 +195,16 @@ class Board {
 class Game{
     private :
         Board b;
-        string pw, pb;
+        string players[2];
         int turn;
         pair<int, int> start, end;
     public :
-        Game(string pw, string pb){this->pw = pw, this->pb = pb, this->turn = 0;}
+        Game(string pw, string pb){this->players[0] = pw, this->players[1] = pb, this->turn = 0;}
         void show(){b.show();}
+        bool check(int a){
+            if(a >= sz || a <0)return 0;
+            return 1;
+        }
         bool getMove(){
             cin >> start.second;
             if(start.second == -1)return 0;
@@ -153,11 +212,26 @@ class Game{
             start.first--, start.second--, end.first--, end.second--;
             return 1;
         }
+        void promotemsg(){
+            cout << "Awesome you can promote now\n What would you like to promote your pawn at " << start.first << " " << start.second << " to promote to?\n";
+            cout << "Options : Rook \n Knight \n Queen \n Bishop \n";
+            cout << "Enter name of piece you want in same case\n";
+        }
         bool move(){
+            if(start == end)return 0;
             Piece * starting_piece = b.cellPtr(start.first, start.second), * ending_piece = b.cellPtr(end.first, end.second);
             if(starting_piece == NULL || (*starting_piece).colour() == turn)return 0;
             if(ending_piece != NULL and (*starting_piece).colour() == (*ending_piece).colour())return 0;
             if((*starting_piece).canMove(b.allCells(), start, end) == 0)return 0;
+            if((*starting_piece).name() == 'p' and end.second == 7){
+                promotemsg();
+                string s;
+                cin >> s;
+                if(s == "Rook")starting_piece = new Rook((*starting_piece).colour());
+                else if(s == "Bishop")starting_piece = new Bishop((*starting_piece).colour());
+                else if(s == "Knight")starting_piece = new Knight((*starting_piece).colour());
+                else if(s == "Queen")starting_piece = new Queen((*starting_piece).colour());
+            }
             b.set(end.first, end.second, starting_piece);
             b.set(start.first, start.second, NULL);
             turn = (turn+1)%2;
@@ -165,10 +239,13 @@ class Game{
         }
         void play(){
             show();
-            cout << "First Move?\n";
+            cout << "First Move " << players[0] << " ?\n";
             while(getMove()){
-                bool x = move();
-                if(x)cout << "Next Move?\n", show();
+                if(check(start.first)&&check(start.second)&&check(end.first)&&check(end.second)){
+                    bool x = move();
+                    if(x)cout << "Next Move " << players[turn] << " ?\n", show();
+                    else cout << "Invalid Move! Think again.\n";
+                }
                 else cout << "Invalid Move! Think again.\n";
             }
         }
